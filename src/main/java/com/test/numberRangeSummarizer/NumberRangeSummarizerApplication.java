43df366713ac447289a1c2a7addce8a62943ad2b
202136272 <bmabulu@webmail.co.za>
2019-07-01 18:10:44 +0200

    Initial commit.

---
+++ src/main/java/com/test/numberRangeSummarizer/NumberRangeSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.test.numberRangeSummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberRangeSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberRangeSummarizerApplication.class, args);
+	}
+
+}
