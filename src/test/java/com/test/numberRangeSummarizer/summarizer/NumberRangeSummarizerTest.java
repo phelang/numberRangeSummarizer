87f7209d6a7dfd0e7515e35ff41bc29c2179e743
202136272 <bmabulu@webmail.co.za>
2019-07-01 23:05:29 +0200

    Add number summarizer test file.

---
+++ src/test/java/com/test/numberRangeSummarizer/summarizer/NumberRangeSummarizerTest.java
@@ -0,0 +1,20 @@
+package com.test.numberRangeSummarizer.summarizer;
+
+import com.test.numberRangeSummarizer.summarizer.impl.NumberRangeSummarizerImpl;
+import org.junit.Test;
+
+public class NumberRangeSummarizerTest {
+
+    private final NumberRangeSummarizer numberRangeSummarizer = new NumberRangeSummarizerImpl();
+
+    @Test
+    public void testThatStringIsSplit() {
+
+
+    }
+
+    @Test
+    public void testThatCollectionIs() {
+
+    }
+}
