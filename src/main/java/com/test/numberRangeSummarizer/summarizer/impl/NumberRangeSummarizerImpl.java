1015624a40179b4b35e45ffeffe16bee855e92c1
202136272 <bmabulu@webmail.co.za>
2019-07-01 18:49:34 +0200

    Create NumberRangeSummarizer implementaion file.

---
+++ src/main/java/com/test/numberRangeSummarizer/summarizer/impl/NumberRangeSummarizerImpl.java
@@ -0,0 +1,24 @@
+package com.test.numberRangeSummarizer.summarizer.impl;
+
+import com.test.numberRangeSummarizer.summarizer.NumberRangeSummarizer;
+
+import java.util.Collection;
+
+/**
+ * @author Qhu
+ *
+ * Implements NumberRangeSummarizer Interface. Accepts comma delimited list of numbers,
+ * and groups the numbers into a range when they are sequential.
+ *
+ */
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
+
+    public Collection<Integer> collect(String input) {
+        return null;
+    }
+
+    public String summarizeCollection(Collection<Integer> input) {
+        return null;
+    }
+}
