7b4707505fc75cd83e3885182d2fc71d43f1f0db
qhu <phelang.qhu@gmail.com>
2019-07-04 18:59:36 +0200

    Create number summarizer recursive algorithm.

--- src/main/java/com/test/numberRangeSummarizer/summarizer/impl/NumberRangeSummarizerImpl.java
+++ src/main/java/com/test/numberRangeSummarizer/summarizer/impl/NumberRangeSummarizerImpl.java
@@ -2,8 +2,7 @@ package com.test.numberRangeSummarizer.summarizer.impl;
 
 import com.test.numberRangeSummarizer.summarizer.NumberRangeSummarizer;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -16,9 +15,13 @@ import java.util.stream.Collectors;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
+    private int indexIncrement = 0;
+    private boolean isSequential;
+    private int currentNumber = 0;
+    private int nextNumber = 0;
+
     @Override
     public final Collection<Integer> collect(String input) {
-
         return Arrays.stream(input.split("[,]"))
                 .filter(val -> val.matches("^[0-9]*$"))
                 .map(Integer::parseInt)
@@ -28,8 +31,35 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
     @Override
     public final String summarizeCollection(Collection<Integer> input) {
-        return "Hello World";
+
+        ArrayList<Integer> list = new ArrayList<>(input);
+        String str = list.get(0).toString();
+
+        return summarize(list, indexIncrement, str);
     }
+
+    private String summarize(ArrayList<Integer> list, int i, String str){
+        currentNumber = (list.get(i) + 1);
+        nextNumber = list.get(i + 1);
+
+        if(currentNumber == nextNumber){
+            i++;
+            isSequential = true;
+        }
+        else if(isSequential) {
+            str += "-" + list.get(i);
+            i++;
+            str += ("," + list.get(i));
+            isSequential = false;
+        }
+        else {
+            isSequential = false;
+            i++;
+            str += "," + list.get(i);
+        }
+        return (i <= list.size() - 2 ? summarize(list, i, str) : str) ;
+    }
+
 
     /* MAYBE OR NOT - Unnecessary repition create a separate file implementation for utils */
     private String[] splitInput(String input) {
