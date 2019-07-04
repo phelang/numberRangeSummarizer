2936e99b6db13fa4b04acf9da2d8e345b7b91816
qhu <phelang.qhu@gmail.com>
2019-07-04 19:19:25 +0200

    Test that input string of numbers is split, in collection form and is sequential

--- src/test/java/com/test/numberRangeSummarizer/summarizer/NumberRangeSummarizerTest.java
+++ src/test/java/com/test/numberRangeSummarizer/summarizer/NumberRangeSummarizerTest.java
@@ -1,20 +1,37 @@
 package com.test.numberRangeSummarizer.summarizer;
 
 import com.test.numberRangeSummarizer.summarizer.impl.NumberRangeSummarizerImpl;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class NumberRangeSummarizerTest {
 
     private final NumberRangeSummarizer numberRangeSummarizer = new NumberRangeSummarizerImpl();
 
-    @Test
-    public void testThatStringIsSplit() {
-
-
+    @Before
+    public void setUp() throws Exception {
     }
 
     @Test
-    public void testThatCollectionIs() {
+    public void testCollectionSize() {
+        Assert.assertEquals(numberRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31").size(), 14);
+    }
 
+    @Test
+    public void testThatStringIsSplit() {
+        Collection<Integer> numbers = numberRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
+        Assert.assertFalse(numbers.isEmpty());
+        Assert.assertEquals(numbers.iterator().hasNext(), true);
+    }
+
+    @Test
+    public void testThatCollectionIsSequential() {
+
+        Collection<Integer> numbers = numberRangeSummarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
+         Assert.assertFalse(Boolean.parseBoolean(Arrays.stream(numbers.toArray()).unordered().toString()));
     }
 }
